aa2bd75beafdcfd83e56fa8083ff6a052f4ca5c4
wazyp <x0xdjdix0x@gmail.com>
2021-05-04 22:53:18 +0300

    Work on JS OOP

--- OOP/itemCreation.js
+++ OOP/itemCreation.js
@@ -34,7 +34,7 @@ function Weapon(attack, damageType, twoHanded, id, name) {
     }
     this.twoHanded = twoHanded;
     this.chance = getRandomInt(5, 50);
-    this.itemInfo = this.itemInfo + this.name + ' has ' + this.attack + ' of ' + this.damageType + ' damage';
+    this.itemInfo = this.itemInfo + ' has ' + this.attack + ' of ' + this.damageType + ' damage';
 }
 
 function Sword(attack, damageType, twoHanded, id, name) {
@@ -80,6 +80,7 @@ function Bow(attack, damageType, twoHanded, id, name, arrowType) {
 }
 
 function Staff(attack, damageType, twoHanded, id, name, arrowType) {
+
     if (damageType == 'physical') {
         throw 'Staff can not have physical type damage';
     }
@@ -89,18 +90,162 @@ function Staff(attack, damageType, twoHanded, id, name, arrowType) {
     }
 
     Weapon.call(this, attack, damageType, twoHanded, id, name);
+
+    this.burn = false;
+    if (damageType == 'fire') {
+        this.burn = true;
+    }
+
+    this.poison = false;
+    if (damageType == 'poison') {
+        this.poison = true;
+    }
+
+    this.cold = false;
+    if (damageType == 'water') {
+        this.cold = true;
+    }
+
+    this.electrify = false;
+    if (damageType == 'air') {
+        this.electrify = true;
+    }
+
+    this.tremor = false;
+    if (damageType == 'earth') {
+        this.tremor = true;
+    }
+
+    this.itemInfo = this.itemInfo + ' and has ' + this.chance + '% to apply ' + (this.burn ? 'burn ' : '') +  (this.poison ? 'poison ' : '') +  (this.cold ? 'cold ' : '') +  (this.electrify ? 'electrify ' : '') +  (this.tremor ? 'tremor ' : '');
+}
+
+function Armor(defense, resistance, id, name) {
+    Item.call(this, id, name);
+
+    if (defense < 1 || defense > 50000) {
+        throw 'Item defense is invalid';
+    }
+    this.defense = defense;
+
+    let validInput = ['physical', 'poison', 'fire', 'water', 'air', 'earth'];
+
+    if (!validInput.includes(resistance)) {
+        throw 'damageType is not valid!';
+    }
+    this.resistance = resistance;
+
+    this.chance = getRandomInt(10, 100);
+
+    this.itemInfo = this.itemInfo + ' has ' + this.defense + ' defense and ' +  this.chance + '% ' + this.resistance + ' resistance ';
+}
+
+function Helm(defense, resistance, id, name, attractivness) {
+    Armor.call(this, defense, resistance, id, name);
+
+    if (attractivness < -5 || attractivness > 5) {
+        throw 'Item attractivness is invalid';
+    }
+    this.attractivness = attractivness;
+
+    this.itemInfo = this.itemInfo + 'and adds ' + this.attractivness + ' attractivness ';
+}
+
+function Boots(defense, resistance, id, name, speed) {
+    Armor.call(this, defense, resistance, id, name);
+
+    if (speed < 1 || speed > 10) {
+        throw 'Item speed is invalid';
+    }
+    this.speed = speed;
+
+    this.itemInfo = this.itemInfo + 'and adds ' + this.speed + ' speed ' ;
+}
+
+function Gloves(defense, resistance, id, name, crafting) {
+    Armor.call(this, defense, resistance, id, name);
+
+    if (crafting < 1 || crafting > 10) {
+        throw 'Item crafting is invalid';
+    }
+    this.crafting = crafting;
+
+    this.itemInfo = this.itemInfo + 'and adds ' + this.crafting + ' crafting ';
+}
+
+function Robe(defense, resistance, id, name, repotation) {
+    Armor.call(this, defense, resistance, id, name);
+
+    if (repotation < 1 || repotation > 10) {
+        throw 'Item repotation is invalid';
+    }
+    this.repotation = repotation;
+
+    this.itemInfo = this.itemInfo + 'and adds ' + this.repotation + ' repotation ';
+}
+
+function Consumable(heals, type, id, name) {
+    Item.call(this, id, name);
+
+    if (typeof heals !== 'boolean') {
+        throw 'heals is not valid!';
+    }
+    this.heals = heals;
+
+    let validInput = ['minor', 'medium', 'big'];
+
+    if (!validInput.includes(type)) {
+        throw 'type is not valid!';
+    }
+    this.type = type;
+
+    this.effect = 0;
+    switch(this.type) {
+        case "minor":
+            this.effect = getRandomInt(1, 10);
+            break;
+        case "medium":
+            this.effect = getRandomInt(11, 20);
+            break;
+        case "big":
+            this.effect = getRandomInt(20, 30);
+            break;
+        default:
+            throw 'effect is not valid!';
+    }
+
+    this.itemInfo = this.itemInfo + ' it is ' + this.type + ' potion and ' + (this.heals ? 'heals' : 'damages')  + ' for ' + this.effect;
+
 }
 
 Weapon.prototype = Object.create(Item.prototype);
 Sword.prototype = Object.create(Weapon.prototype);
 Bow.prototype = Object.create(Weapon.prototype);
+Staff.prototype = Object.create(Weapon.prototype);
+Armor.prototype = Object.create(Item.prototype);
+Helm.prototype = Object.create(Armor.prototype);
+Boots.prototype = Object.create(Armor.prototype);
+Gloves.prototype = Object.create(Armor.prototype);
+Robe.prototype = Object.create(Armor.prototype);
+Consumable.prototype = Object.create(Item.prototype);
 
 let pushka = new Weapon(20, 'fire', true, 1, 'pushkaa');
 let shmushka = new Weapon(1000, 'poison', false, 2, 'shmushkaaa');
 let pruchka = new Sword(50, 'physical', true, 69, 'pruchka');
-let prashka = new Bow(50, 'physical', true, 29, 'prashka', 'special');
+let prashka = new Bow(69, 'physical', true, 29, 'prashka', 'special');
+let bubble = new Staff(80, 'fire', true, 31, 'bubble', 'special');
+let shmaska = new Helm(80, 'water', 35, 'shmaska', 5);
+let gucciCheili = new Boots(20000, 'poison', 36, 'gucci cheili', 5);
+let hendurs = new Gloves(200, 'earth', 37, 'hendurs', 10);
+let halat = new Robe(100, 'air', 38, 'halat', 4);
+let helche = new Consumable(true, 'big', 39, 'helche');
 
 console.log(pushka.getItemInfo());
 console.log(shmushka.getItemInfo());
 console.log(pruchka.getItemInfo());
 console.log(prashka.getItemInfo());
+console.log(bubble.getItemInfo());
+console.log(shmaska.getItemInfo());
+console.log(gucciCheili.getItemInfo());
+console.log(hendurs.getItemInfo());
+console.log(halat.getItemInfo());
+console.log(helche.getItemInfo());
